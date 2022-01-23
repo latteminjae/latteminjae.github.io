4804ef8a5ffcd394d4dffa1f5ece4500b4b39e05
minjae.kim <mjkl7896@gmail.com>
2022-01-23 17:00:29 +0900

    feat : launching

--- niro_ hUIA0MzfFo/js/functions.ts
+++ niro_ hUIA0MzfFo/js/functions.ts
@@ -1,19 +1,15 @@
 import gsap, { TweenMax } from "gsap";
 
 // type === "intro" && typeof index === "number" 인 케이스는 없다.
-export function animate(type: "intro" | HTMLElement, index?: number) {
-    if (type === "intro" && index === undefined) {
-        const bg = document.querySelector("#ms_niro-prelaunching-wrap .mainBg .tween1") as HTMLElement;
-        const tweens = document.querySelectorAll("#ms_niro-prelaunching-wrap .introSection .tween1");
+export function animate(type: "intro" | HTMLElement) {
+    const wrap = document.getElementById("ms_niro-launching-wrap") as HTMLElement;
 
-        TweenMax.to(bg, 0.5, {
-            opacity: 0.5,
-            top: 0,
-            delay: 0.25,
-        });
+    if (type === "intro") {
+        const fixedTweens = wrap.querySelectorAll(".fixedBox .tween1");
+        const sectionTweens = wrap.querySelectorAll(".sectionSlideBox .section:last-of-type .tween1");
 
         TweenMax.staggerTo(
-            tweens,
+            fixedTweens,
             0.9,
             {
                 opacity: 1,
@@ -23,42 +19,148 @@ export function animate(type: "intro" | HTMLElement, index?: number) {
             0.4
         );
 
-        return;
-    }
-
-    if (type !== "intro") {
-        let tweens = type.querySelectorAll(".tween1") as NodeListOf<HTMLElement>;
-
-        // film 모바일의 경우 아이템 애니메이션 순서가 바뀌어서 별도분기 작성.
-        if (type.querySelector(".movieBox") && window.innerWidth < 1025) {
-            const title = ".sectionTitle.tween1";
-            const lastItem = ".sectionContent > .flex > .row:last-of-type .tween1";
-            const otherItem = ".sectionContent > .flex > .row:first-of-type .tween1";
-
-            tweens = type.querySelectorAll(`${title}, ${lastItem}`);
-            const tweens2 = type.querySelectorAll(otherItem);
-
-            TweenMax.staggerTo(
-                [tweens, tweens2],
-                0.5,
-                {
-                    opacity: 1,
-                    top: 0,
-                },
-                0.3
-            );
-
-            return;
-        }
-
         TweenMax.staggerTo(
-            tweens,
-            0.5,
+            sectionTweens,
+            0.9,
             {
                 opacity: 1,
                 top: 0,
             },
-            0.3
+            0.4
         );
+
+        return;
     }
+
+    let tweens = type.querySelectorAll(".tween1") as NodeListOf<HTMLElement>;
+
+    TweenMax.staggerTo(
+        tweens,
+        0.5,
+        {
+            opacity: 1,
+            top: 0,
+            delay: 0.3,
+        },
+        0.3
+    );
+}
+
+export function scrollDirectionIsUp(event: any) {
+    if (event.wheelDelta) return event.wheelDelta > 0;
+    return event.deltaY < 0;
+}
+
+function tweenInit() {
+    const wrap = document.getElementById("ms_niro-launching-wrap") as HTMLElement;
+    const tweens = wrap.querySelectorAll(".section .tween1") as NodeListOf<HTMLElement>;
+    tweens.forEach((v) => v.setAttribute("style", ""));
+}
+
+let isSliding = false;
+export function slideUp(sections: NodeListOf<HTMLElement>, listItems: NodeListOf<HTMLElement>): void {
+    if (isSliding) return;
+
+    const length = sections.length;
+    if (sections[1].classList.contains("top")) return;
+
+    isSliding = true;
+    let index: number = 0;
+    for (let i = 0; i < length; i++) {
+        if (sections[i].classList.contains("top")) {
+            index = i - 1;
+            break;
+        }
+    }
+
+    listItems.forEach((v) => {
+        v.classList.remove("on");
+    });
+
+    if (index === 0) {
+        sections[length - 1 - index].classList.add("top");
+        tweenInit();
+        animate(sections[length - 2 - index]);
+    } else if (index > 0) {
+        sections[index].classList.add("top");
+        tweenInit();
+        animate(sections[index - 1]);
+    }
+
+    if (sections[1].classList.contains("top")) {
+        listItems[4].classList.add("on");
+    } else if (index === 0) {
+        listItems[1].classList.add("on");
+    } else {
+        listItems[length - index].classList.add("on");
+    }
+
+    setTimeout(() => {
+        isSliding = false;
+    }, 700);
+}
+
+export function slideDown(sections: NodeListOf<HTMLElement>, listItems: NodeListOf<HTMLElement>): void {
+    if (isSliding) return;
+
+    const length = sections.length;
+    if (!sections[length - 1].classList.contains("top")) return;
+
+    isSliding = true;
+    let index: number = 0;
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].classList.contains("top")) {
+            index = i;
+            break;
+        }
+    }
+
+    listItems.forEach((v) => {
+        v.classList.remove("on");
+    });
+
+    if (!sections[length - 1].classList.contains("top")) {
+        listItems[0].classList.add("on");
+    } else {
+        listItems[length - index - 1].classList.add("on");
+    }
+
+    sections[index].classList.remove("top");
+    tweenInit();
+    animate(sections[index]);
+
+    setTimeout(() => {
+        isSliding = false;
+    }, 700);
+}
+
+export function slideTo(sections: NodeListOf<HTMLElement>, listItems: NodeListOf<HTMLElement>, index: number): void {
+    if (isSliding) return;
+
+    isSliding = true;
+    const sectionIndex = sections.length - 1 - index;
+
+    tweenInit();
+
+    sections.forEach((v, i) => {
+        if (i <= sectionIndex) {
+            v.classList.remove("top");
+        } else {
+            v.classList.add("top");
+        }
+    });
+
+    listItems.forEach((v, i) => {
+        if (i === index) {
+            v.classList.add("on");
+        } else {
+            v.classList.remove("on");
+        }
+    });
+
+    animate(sections[sectionIndex]);
+
+    setTimeout(() => {
+        isSliding = false;
+    }, 700);
 }
--- niro_ hUIA0MzfFo/js/index.ts
+++ niro_ hUIA0MzfFo/js/index.ts
@@ -13,112 +13,60 @@ interface Scene_ extends Scene {
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 window.onload = function () {
-    const wrap = document.getElementById("ms_niro-prelaunching-wrap") as HTMLElement;
+    const wrap = document.getElementById("ms_niro-launching-wrap") as HTMLElement;
+
+    const ms_container = wrap.querySelector(".ms_container") as HTMLElement;
+    const sectionSlideBox = wrap.querySelector(".sectionSlideBox") as HTMLElement;
+    const sections = sectionSlideBox.querySelectorAll(".section") as NodeListOf<HTMLElement>;
+    const listItems = wrap.querySelectorAll(".fixedBox .listBox ul > li") as NodeListOf<HTMLElement>;
 
     // scrollmagic
     const controller = new ScrollMagic.Controller();
 
-    const intro = wrap.querySelector(".introSection") as HTMLElement;
-    const sections = wrap.querySelectorAll(".contentSection > .section") as NodeListOf<HTMLElement>;
-
-    (
-        new ScrollMagic.Scene({
-            duration: 1,
-            offset: -250,
-            triggerElement: intro,
-        }) as Scene_
-    )
-        .on("start", function (e) {
-            func.animate("intro");
-        })
-        .addTo(controller);
-
-    sections.forEach((v, i) => {
-        (
-            new ScrollMagic.Scene({
-                duration: 1,
-                offset: v.getBoundingClientRect().height / 2,
-                triggerElement: v,
-                triggerHook: 1,
-            }) as Scene_
-        )
-            .on("start", function (e) {
-                func.animate(v, i);
-            })
-            .addTo(controller);
-    });
-
-    // contentBox up/down
-    const introSection = wrap.querySelector(".introSection") as HTMLElement;
-    const contentBox = wrap.querySelector(".contentBox") as HTMLElement;
-    const contentSection = wrap.querySelector(".contentSection") as HTMLElement;
-    const closeBtn = wrap.querySelectorAll(".closeBtn") as NodeListOf<HTMLElement>;
-
-    function layerOpen() {
-        if (!contentBox.classList.contains("on")) {
-            contentBox.classList.add("on");
-            contentSection.scrollTop = 0;
-        }
-    }
-
-    function layerOff() {
-        if (contentBox.classList.contains("on")) {
-            contentBox.classList.remove("on");
-        }
-    }
-
-    ["scroll", "mousewheel", "wheel", "touchmove"].forEach((v) => {
-        introSection.addEventListener(v, (e) => {
+    // sections 스크롤 슬라이드
+    ["scroll", "mousewheel", "wheel"].forEach((v) => {
+        ms_container.addEventListener(v, (e) => {
             e.preventDefault();
-            layerOpen();
+            const isUp = func.scrollDirectionIsUp(e);
+            if (isUp) {
+                func.slideDown(sections, listItems);
+            } else {
+                func.slideUp(sections, listItems);
+            }
         });
     });
 
-    closeBtn.forEach((v) => {
-        v.addEventListener("click", () => {
-            layerOff();
-        });
+    let lastY = 0;
+    ms_container.addEventListener("touchmove", (e) => {
+        const currentY = e.touches[0].clientY;
+        if (currentY > lastY) {
+            func.slideDown(sections, listItems);
+        } else if (currentY < lastY) {
+            func.slideUp(sections, listItems);
+        }
+        lastY = currentY;
     });
 
-    // feature 버튼 클릭 - 전환
-    const featureBtns = wrap.querySelectorAll(".featureBox .toggleBtnBox button") as NodeListOf<HTMLElement>;
-    const featureListPC = wrap.querySelectorAll(".featureBox .ms_nontab .featureList") as NodeListOf<HTMLElement>;
-    const featureListMO = wrap.querySelectorAll(".featureBox .ms_tab .featureList") as NodeListOf<HTMLElement>;
-    featureBtns.forEach((v, i) => {
+    // section list click -> slide
+    listItems.forEach((v, i) => {
         v.addEventListener("click", () => {
-            if (v.classList.contains("on")) return;
-
-            featureBtns.forEach((v1) => v1.classList.remove("on"));
-            featureListPC.forEach((v1) => v1.classList.remove("on"));
-            featureListMO.forEach((v1) => v1.classList.remove("on"));
-
-            v.classList.add("on");
-            featureListPC[i].classList.add("on");
-            featureListMO[i].classList.add("on");
+            func.slideTo(sections, listItems, i);
         });
     });
 
     // modal
     const modalWrapper = wrap.querySelector(".modalWrapper") as HTMLElement;
     const modalScrollBox = wrap.querySelector(".contentDiv") as HTMLElement;
-    const modalImages = wrap.querySelectorAll(".modalWrapper .contentList img") as NodeListOf<HTMLImageElement>;
-    const slideImages = wrap.querySelectorAll(".slideBox .slideItem > img") as NodeListOf<HTMLImageElement>;
+    const galleryBtn = wrap.querySelector(".galleryBtn") as HTMLElement;
 
     // modal open
-    slideImages.forEach((v) => {
-        v.addEventListener("click", () => {
-            const index = Number(v.dataset.gallery as string);
-
-            if (!modalWrapper.classList.contains("on")) {
-                modalWrapper.classList.add("on");
-                window.scrollTo({ top: 0 });
-                modalScrollBox.scrollTop = modalImages[index].offsetTop - 100;
-            }
-        });
+    galleryBtn.addEventListener("click", () => {
+        modalWrapper.classList.add("on");
+        modalScrollBox.scrollTop = 0;
     });
 
     // modal close
-    modalWrapper?.addEventListener("click", () => {
+    modalWrapper.addEventListener("click", () => {
         modalWrapper.classList.remove("on");
     });
 
@@ -141,19 +89,6 @@ window.onload = function () {
         movieIframe.src = "";
     });
 
-    // 앵커
-    const hash = window.location.hash.slice(1);
-    const sectionsName = ["film", "feature", "gallery", "order"];
-    if (sectionsName.some((v) => v === hash)) {
-        layerOpen();
-
-        const index = sectionsName.indexOf(hash);
-        console.log(index);
-        contentSection.scrollTop = sections[index].offsetTop - 42;
-
-        console.log("asdf");
-    }
-
     // 모바일 브라우저 상단 navigation공간까지 합해서 vh를 계산하기 위해 사용
     // css에서 다음과 같이 사용 -> height: var(--vh);
     let vh = window.innerHeight * 0.01;
@@ -162,4 +97,7 @@ window.onload = function () {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
     });
+
+    // init animation
+    func.animate("intro");
 };
